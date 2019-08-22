2d30a7aaef37fd0191fdc5610f60eb193f950f45
Juan C. Diaz <me@jucardi.com>
2019-08-21 19:46:02 -0700

    added json marshaller, so both json and yaml can be used

--- testx/utils.go
+++ testx/utils.go
@@ -1,14 +1,23 @@
 package testx
 
 import (
+	"encoding/json"
 	"fmt"
 	"gopkg.in/yaml.v2"
 	"io"
 	"os"
 )
 
-func Marshal(obj interface{}) string {
+func MarshalYAML(obj interface{}) string {
 	return marshalObj(obj, yaml.Marshal)
+}
+
+func MarshalJSON(obj interface{}) string {
+	return marshalObj(obj, json.Marshal())
+}
+
+func MarshalJSONIndent(obj interface{}) string {
+	return marshalObj(obj, func(o interface{}) ([]byte, error) { return json.MarshalIndent(o, "", "  ") })
 }
 
 func PrintObj(msg string, obj interface{}, writer ...io.Writer) {
@@ -16,7 +25,7 @@ func PrintObj(msg string, obj interface{}, writer ...io.Writer) {
 	if len(writer) > 0 && writer[0] != nil {
 		w = writer[0]
 	}
-	_, _ = fmt.Fprintf(w, "%s   (Type: %T)\n%s\n", msg, obj, Marshal(obj))
+	_, _ = fmt.Fprintf(w, "%s   (Type: %T)\n%s\n", msg, obj, MarshalYAML(obj))
 }
 
 func marshalObj(obj interface{}, handler func(interface{}) ([]byte, error)) string {
