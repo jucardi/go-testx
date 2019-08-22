e104d11282cd940f7a390baa96c911dc871bc26d
Juan C. Diaz <me@jucardi.com>
2019-08-21 19:43:31 -0700

    changed the marshaller to yaml

--- testx/utils.go
+++ testx/utils.go
@@ -1,18 +1,14 @@
 package testx
 
 import (
-	"encoding/json"
 	"fmt"
+	"gopkg.in/yaml.v2"
 	"io"
 	"os"
 )
 
 func Marshal(obj interface{}) string {
-	return marshalObj(obj, json.Marshal)
-}
-
-func MarshalIndent(obj interface{}) string {
-	return marshalObj(obj, func(o interface{}) ([]byte, error) { return json.MarshalIndent(o, "", "  ") })
+	return marshalObj(obj, yaml.Marshal)
 }
 
 func PrintObj(msg string, obj interface{}, writer ...io.Writer) {
@@ -20,7 +16,7 @@ func PrintObj(msg string, obj interface{}, writer ...io.Writer) {
 	if len(writer) > 0 && writer[0] != nil {
 		w = writer[0]
 	}
-	_, _ = fmt.Fprintf(w, "%s   (Type: %T)\n%s\n", msg, obj, MarshalIndent(obj))
+	_, _ = fmt.Fprintf(w, "%s   (Type: %T)\n%s\n", msg, obj, Marshal(obj))
 }
 
 func marshalObj(obj interface{}, handler func(interface{}) ([]byte, error)) string {
